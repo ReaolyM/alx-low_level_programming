283685ba884e8e90e5d434e4d48ac8c1c57c9e9a
ReaolyM <rme.manamela@gmail.com>
2023-04-09 12:04:02 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,11 +3,12 @@
  *_strpbrk - Entry point
  *@s: input
  *@accept: input
- *Return: Always 0
+ *Return: Always 0 (Success)
 */
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept)
 {
 int k;
+
 while (*s)
 {
 for (k = 0; accept[k]; k++)
