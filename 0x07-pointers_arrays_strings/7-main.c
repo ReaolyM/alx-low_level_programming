3fc0ac58d7cc4082eb1c7b2b28ef53769a23c9fe
ReaolyM <rme.manamela@gmail.com>
2023-04-10 22:56:39 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-main.c
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
+ *main - check the code
  *
- * Return: Always 0.
- */
+ *Return: Always 0.
+*/
 int main(void)
 {
-    char board[8][8] = {
+char board[8][8] = {
         {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
         {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -17,7 +16,7 @@ int main(void)
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
         {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-    };
-    print_chessboard(board);
-    return (0);
+};
+print_chessboard(board);
+return (0);
 }
